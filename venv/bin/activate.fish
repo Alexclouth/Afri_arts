3d80b6091583dfaa45d3c48e03b3037ee6e14a07
Alexclouth <alazeralphilo@example.com>
2024-09-19 18:18:30 +0000

    f

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/lazarus/Afri_arts/venv"
+set -gx VIRTUAL_ENV "/home/ubuntu/Afri_arts/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
