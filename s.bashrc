daba9902758544b8b0b76e8ff35a17d970d005a9
Alexclouth <alazeralphilo@example.com>
2024-09-19 14:03:58 +0300

    afri

--- s.bashrc
+++ s.bashrc
@@ -1,3 +1,4 @@
+export DATABASE_URL="mysql+mysqldb://afri_dev:Ala123.,@localhost/afri_db"
+export SECRET_KEY="5791628bb0b13ce0c676dfde280ba245"
 export MAIL_USERNAME="alazergk@gmail.com"
 export MAIL_PASSWORD="relo dopq wtdu kglh"
-source ~/.bashrc
--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -6,9 +6,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_mail import Mail
-<<<<<<< HEAD
 from web_flask.config import Config
-
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -17,22 +15,30 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.signin'
 login_manager.login_message_category = 'info'
 mail = Mail()
-
-migrate = Migrate(db)
-
-
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    
+    # Initialize extensions with the app
     db.init_app(app)
     bcrypt.init_app(app)
     csrf.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
 
+    # Additional config options
+    app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://afri_dev:Ala123.,@localhost/afri_db'
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+    # Register blueprints
     from web_flask.users.routes import users
     from web_flask.posts.routes import posts
     from web_flask.main.routes import main
@@ -41,34 +47,5 @@ def create_app(config_class=Config):
     app.register_blueprint(posts)
     app.register_blueprint(main)
     app.register_blueprint(errors)
-=======
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://afri_dev:Ala123.,@localhost/afri_db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-csrf = CSRFProtect(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'users.signin'
-login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-mail = Mail(app)
->>>>>>> bd7c580235255908c4cca384db881c411e0799b9
 
     return app
-
-<<<<<<< HEAD
-=======
-from web_flask.users.routes import users
-from web_flask.posts.routes import posts
-from web_flask.main.routes import main
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
-
->>>>>>> bd7c580235255908c4cca384db881c411e0799b9
